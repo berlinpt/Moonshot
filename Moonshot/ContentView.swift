445b37dbfd11924ad961fe729be850fd23bbf83c
Berlin <berlinpt@gmail.com>
2024-07-13 15:15:26 -0400

    Initial Commit

---
+++ Moonshot/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Moonshot
+//
+//  Created by Berlin Thomas on 2024-07-13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Berlin Thomas on 2024-07-13.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
