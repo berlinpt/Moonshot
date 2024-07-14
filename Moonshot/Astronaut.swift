283f78baeafbd905194038e8bad2bfbe1cde952a
Berlin <berlinpt@gmail.com>
2024-07-14 01:51:05 -0400

    Loading a specific kind of Codable data

---
+++ Moonshot/Astronaut.swift
@@ -0,0 +1,15 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Berlin Thomas on 2024-07-14.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+    
+}
